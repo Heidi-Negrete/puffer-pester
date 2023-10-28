d5d8b6488c40c6436b13b0e6e7af6ad808bb4536
Heidi Negrete <heidi@negrete.dev>
2023-10-28 16:06:26 -0400

    update gitignore to ignore symlinks

--- project/scenes/enemy.gd
+++ project/scenes/enemy.gd
@@ -2,8 +2,15 @@ extends RigidBody2D
 
 
 func _ready():
-	$AnimatedSprite2D.scale = randi() % 2 + 1
+	scale = Vector2(randf() + 1, randf() + 1)
 	$AnimatedSprite2D.play()
+
+
+func _process(_delta):
+	if position.x < 0:
+		$AnimatedSprite2D.flip_v = true
+	else:
+		$AnimatedSprite2D.flip_v = false
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
